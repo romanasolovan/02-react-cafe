4bfb7354e1318d984d047467e2e1faba1ae4ecde
Romana Solovan <solo.rv95@gmail.com>
2025-08-14 18:26:55 +0300

    update the folder issues

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import css from "./App.module.css"
 import CafeInfo from "../CafeInfo/CafeInfo";
+import Notification from "../Notification/Notification";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
-import Notification from "../Notification/Notification";
 import type { VoteType, Votes } from "../../types/Votes";
 
 
